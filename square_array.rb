88dcf4119497349262d3fd2dd89e794bb2c592d1
Bartholomew Fountain <baertholomew@gmail.com>
2020-03-12 03:51:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,4 +4,9 @@ counter = 0
 
 while counter < array.length do
   new_arroy <<array[counter] ** 2
-  counter += 
+  counter += 1
+  #counter = counter + 1
+end
+##return the new array
+new_array
+end
